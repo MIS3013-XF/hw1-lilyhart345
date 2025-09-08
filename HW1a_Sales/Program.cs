6037188eb289cbe1d87c83f751cc73cfad7b2654
Lily Hart <lily.d.hart-1@ou.edu>
2025-09-08 13:05:18 -0500

    commit

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -15,6 +15,28 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            double subTotal = 0;
+            double salesTax = 0;
+            double salesTotal = 0;
+            const double taxRate = .085;
+            string typeOfItem = "";
+            int quantity = 0;
+            double priceOfItem = 0;
+
+            Console.WriteLine("Enter the type of item you are purchasing");
+            typeOfItem = Console.ReadLine();
+            Console.WriteLine("How many " + typeOfItem + "'s do you want to buy?");
+            quantity = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the price for " + typeOfItem + "?");
+            priceOfItem = double.Parse(Console.ReadLine());
+
+            subTotal = priceOfItem * quantity;
+            salesTax = subTotal * taxRate;
+            salesTotal = subTotal + salesTax;
+
+            Console.WriteLine("\tYour subtotal for you bill is " + subTotal.ToString("C"));
+            Console.WriteLine("\tYour sales tax for you bill is " + salesTax.ToString("C"));
+            Console.WriteLine("\tYour total for you bill is " + salesTotal.ToString("C"));
         }
     }
 }
